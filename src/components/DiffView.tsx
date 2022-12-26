59d9c93b43145aad5015b2624d94c2911cd335b6
me <tkutlesa@gmail.com>
2022-12-26 12:27:33 -0500

    remove diff viewer

--- src/components/DiffView.tsx
+++ src/components/DiffView.tsx
@@ -1,16 +1,8 @@
-import ReactDiffViewer, { DiffMethod } from "react-diff-viewer";
 import { text1Atom, text2Atom } from "../store";
 import { useAtom } from "jotai";
 
 export default function DiffView() {
   const [text1, setText1] = useAtom(text1Atom);
   const [text2, setText2] = useAtom(text2Atom);
-  return (
-    <ReactDiffViewer
-      oldValue={text1}
-      newValue={text2}
-      splitView={true}
-      compareMethod={DiffMethod.WORDS}
-    />
-  );
+  return <>diff</>;
 }
